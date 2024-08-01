13f82bc49066668287bd04d9df371f0cc4c35081
yanghw <hongweiyang2@zjsaisi.com>
2024-08-01 14:29:03 +0800

    V7.4 创建camera类；

--- src/main.d
+++ src/main.d
@@ -175,4 +175,5 @@ src/main.o: src/main.cpp include/glad/glad.h include/KHR/khrplatform.h \
  include/glm/gtc/../gtc/../ext/vector_int1_sized.hpp \
  include/glm/gtc/../gtc/../ext/vector_uint1.hpp \
  include/glm/gtc/../gtc/../ext/vector_uint1_sized.hpp \
- include/glm/gtc/type_ptr.inl include/stb_image.h src/shader_s.h
+ include/glm/gtc/type_ptr.inl include/stb_image.h src/shader_s.h \
+ src/camera.h
