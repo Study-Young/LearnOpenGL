09c8e1b8c6b532d58def11b1fdf8a1ccc0f3aa88
yanghw <hongweiyang2@zjsaisi.com>
2024-08-09 10:56:57 +0800

    V28.3 点光源阴影：PCF-2；

--- src/shaderfile/point_shadows.fs
+++ src/shaderfile/point_shadows.fs
@@ -16,6 +16,14 @@ uniform vec3 viewPos;
 uniform float far_plane;
 uniform bool shadows;
 
+vec3 sampleOffsetDirections[20] = vec3[]
+(
+   vec3( 1,  1,  1), vec3( 1, -1,  1), vec3(-1, -1,  1), vec3(-1,  1,  1), 
+   vec3( 1,  1, -1), vec3( 1, -1, -1), vec3(-1, -1, -1), vec3(-1,  1, -1),
+   vec3( 1,  1,  0), vec3( 1, -1,  0), vec3(-1, -1,  0), vec3(-1,  1,  0),
+   vec3( 1,  0,  1), vec3(-1,  0,  1), vec3( 1,  0, -1), vec3(-1,  0, -1),
+   vec3( 0,  1,  1), vec3( 0, -1,  1), vec3( 0, -1, -1), vec3( 0,  1, -1)
+);
 
 float ShadowCalculation(vec3 fragPos)
 {
@@ -29,23 +37,18 @@ float ShadowCalculation(vec3 fragPos)
     float currentDepth = length(fragToLight);
     // test for shadows
     float shadow = 0.0;
-    float bias = 0.05; 
-    float samples = 4.0;
-    float offset = 0.1;
-    for(float x = -offset; x < offset; x += offset / (samples * 0.5))
+    float bias = 0.15;
+    int samples = 20;
+    float viewDistance = length(viewPos - fragPos);
+    float diskRadius = (1.0 + (viewDistance / far_plane)) / 25.0;
+    for(int i = 0; i < samples; ++i)
     {
-        for(float y = -offset; y < offset; y += offset / (samples * 0.5))
-        {
-            for(float z = -offset; z < offset; z += offset / (samples * 0.5))
-            {
-                float closestDepth = texture(depthMap, fragToLight + vec3(x, y, z)).r; 
-                closestDepth *= far_plane;   // Undo mapping [0;1]
-                if(currentDepth - bias > closestDepth)
-                    shadow += 1.0;
-            }
-        }
+        float closestDepth = texture(depthMap, fragToLight + sampleOffsetDirections[i] * diskRadius).r;
+        closestDepth *= far_plane;   // Undo mapping [0;1]
+        if(currentDepth - bias > closestDepth)
+            shadow += 1.0;
     }
-    shadow /= (samples * samples * samples);
+    shadow /= float(samples);
         
     return shadow;
 }
